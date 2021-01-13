e13a73d7a44df46c9ecd65eafb9130e9bdff7c63
agivdel <agph@yandex.ru>
2021-01-13 10:19:33 +0300

    find duplicate files by last modified time and file size

--- src/main/java/agivdel/copiedFilesSearch/Searcher.java
+++ src/main/java/agivdel/copiedFilesSearch/Searcher.java
@@ -12,74 +12,89 @@ import java.util.stream.Stream;
 //сравнение имен - ненадежно
 //сравнение расширений - ненадежно
 //сравнение размеров - ненадежно
-//сравнение побитово - ненадежно
+//сравнение побайтово - ненадежно
 //сравнение времени последнего изменения
 
 public class Searcher {
 
     public void run() throws IOException {
-        String selectedDirectory = directorySelect();
-        iterationFilesFrom(selectedDirectory);
+        while (true) {
+            System.out.println("Для поиска скопированных файлов введите адрес директории поиска, для выхода нажмите z:");
+            String selectedDirectory = input();
+            List<File> fileList = iterationFilesFrom(selectedDirectory);
+            System.out.println("Число файлов в данной директории: " + fileList.size());
+            System.out.println(fileList);
+            List<List<Path>> timeDoubles = findTimeDoubles(fileList);
+            List<List<List<Path>>> sizeDoubles = findSizeDoubles(timeDoubles);
+            printThis(sizeDoubles);
+        }
     }
 
-    private String directorySelect() {
+    private void printThis(List<List<List<Path>>> sizeDoubles) throws IOException {
+        for (List<List<Path>> oneTime : sizeDoubles) {
+            if (oneTime.isEmpty()) continue;
+            System.out.println("Время " + Files.getLastModifiedTime(oneTime.get(0).get(0)));
+            for (List<Path> sizes : oneTime) {
+                if (sizes.isEmpty()) continue;
+                System.out.println("- Размер " + Files.size(sizes.get(0)) + " bytes");
+                sizes.stream().map(Path::getFileName).forEach(System.out::println);
+            }
+        }
+        System.out.println("\nВсего копий различных файлов: " + sizeDoubles.size());
+    }
+
+    private String input() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Для поиска скопированных файлов введите адрес директории поиска:");
             String selectedDirectory = scanner.nextLine();
+            if (selectedDirectory.equals("z")) {
+                System.exit(0);
+            }
             if (Files.exists(Path.of(selectedDirectory))) {
                 return selectedDirectory;
             }
         }
     }
 
-    private void iterationFilesFrom(String selectedDirectory) throws IOException {
+    private List<File> iterationFilesFrom(String selectedDirectory) throws IOException {
         List<File> fileList = new ArrayList<>();
         try (Stream<Path> pathStream = Files.walk(Paths.get(selectedDirectory))) {
-            fileList = pathStream
-                    .filter(Files::isRegularFile)
-                    .map(Path::toFile)
-                    .collect(Collectors.toList());
+            fileList = pathStream.filter(Files::isRegularFile).map(Path::toFile).collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
             //TODO дописать обработку исключения
         }
-        System.out.println("Число файлов в данной директории: " + fileList.size());
-        //Вариант №1, с мапой времени и списка имен файлов
-        findDuplicates(fileList);
-        System.out.println("____________");
-        //Вариант №2, с дополнительным классом Doubles(Doubles имеет 2 поля: время и список имен дублей)
-        findDuplicates2(fileList);
+        return fileList;
     }
 
-    private void findDuplicates(List<File> fileList) throws IOException {
-        Map<FileTime, List<String>> fileMap = new HashMap<>();
-        List<String> fileNameList;
+    private List<List<Path>> findTimeDoubles(List<File> fileList) throws IOException {
+        Map<FileTime, List<Path>> timeFileMap = new HashMap<>();
+        List<Path> filePathList;
         for (File file : fileList) {
             FileTime fileTime = Files.getLastModifiedTime(file.toPath());
-            fileMap.putIfAbsent(fileTime, new ArrayList<>());
-            fileNameList = fileMap.get(fileTime);
-            fileNameList.add(file.getName());
-            fileMap.put(fileTime, fileNameList);
+            timeFileMap.putIfAbsent(fileTime, new ArrayList<>());
+            filePathList = timeFileMap.get(fileTime);
+            filePathList.add(file.toPath());
+            timeFileMap.put(fileTime, filePathList);
         }
-        Map<FileTime, List<String>> doubleFiles = fileMap.entrySet()
-                .stream()
-                .filter(l -> l.getValue().size() > 1)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-        doubleFiles.entrySet().forEach(System.out::println);
+        return timeFileMap.values().stream().filter(l -> l.size() > 1).collect(Collectors.toList());
     }
 
-    private void findDuplicates2(List<File> fileList) throws IOException {
-        Map<FileTime, Doubles> doublesMap = new HashMap<>();
-        for (File file : fileList) {
-            FileTime fileTime = Files.getLastModifiedTime(file.toPath());
-            Doubles doubles = new Doubles(fileTime, List.of(file.getName()));
-            doublesMap.merge(fileTime, doubles, Doubles::mergeNames);
+    private List<List<List<Path>>> findSizeDoubles(List<List<Path>> timeDoubles) throws IOException {
+        List<List<List<Path>>> sizeDoubles = new ArrayList<>();
+        for (List<Path> oneTimeDoubles : timeDoubles) {
+            Map<Long, List<Path>> sizeMap = new HashMap<>();
+            List<Path> oneSizePaths;
+            for (Path path : oneTimeDoubles) {
+                long size = Files.size(path);
+                sizeMap.putIfAbsent(size, new ArrayList<>());
+                oneSizePaths = sizeMap.get(size);
+                oneSizePaths.add(path);
+                sizeMap.put(size, oneSizePaths);
+            }
+            List<List<Path>> sizes = sizeMap.values().stream().filter(l -> l.size() > 1).collect(Collectors.toList());
+            sizeDoubles.add(sizes);
         }
-        doublesMap.values()
-                .stream()
-                .filter(d -> d.getDoubles().size() > 1)
-                .forEach(System.out::println);
+        return sizeDoubles;
     }
-
 }
