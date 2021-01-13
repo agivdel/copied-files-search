43cabd511dea72f7f8a9f732c483dfdb1c25d879
agivdel <agph@yandex.ru>
2021-01-13 15:24:44 +0300

    rewrite time duplicates searching with stream

--- src/main/java/agivdel/copiedFilesSearch/Searcher.java
+++ src/main/java/agivdel/copiedFilesSearch/Searcher.java
@@ -4,16 +4,9 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.FileTime;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-//сравнение имен - ненадежно
-//сравнение расширений - ненадежно
-//сравнение размеров - ненадежно
-//сравнение побайтово - ненадежно
-//сравнение времени последнего изменения
 
 public class Searcher {
 
@@ -23,24 +16,23 @@ public class Searcher {
             String selectedDirectory = input();
             List<File> fileList = iterationFilesFrom(selectedDirectory);
             System.out.println("Число файлов в данной директории: " + fileList.size());
-            System.out.println(fileList);
-            List<List<Path>> timeDoubles = findTimeDoubles(fileList);
-            List<List<List<Path>>> sizeDoubles = findSizeDoubles(timeDoubles);
+            List<List<File>> timeDoubles = getTimeDoubles(fileList);
+            List<List<List<File>>> sizeDoubles = getSizeDoubles(timeDoubles);
             printThis(sizeDoubles);
         }
     }
 
-    private void printThis(List<List<List<Path>>> sizeDoubles) throws IOException {
-        for (List<List<Path>> oneTime : sizeDoubles) {
+    private void printThis(List<List<List<File>>> sizeDoubles) throws IOException {
+        for (List<List<File>> oneTime : sizeDoubles) {
             if (oneTime.isEmpty()) continue;
-            System.out.println("Время " + Files.getLastModifiedTime(oneTime.get(0).get(0)));
-            for (List<Path> sizes : oneTime) {
+            System.out.println("Время " + Files.getLastModifiedTime(oneTime.get(0).get(0).toPath()));
+            for (List<File> sizes : oneTime) {
                 if (sizes.isEmpty()) continue;
-                System.out.println("- Размер " + Files.size(sizes.get(0)) + " bytes");
-                sizes.stream().map(Path::getFileName).forEach(System.out::println);
+                System.out.println("- Размер " + Files.size(sizes.get(0).toPath()) + " bytes");
+                sizes.stream().map(File::getName).forEach(System.out::println);
             }
         }
-        System.out.println("\nВсего копий различных файлов: " + sizeDoubles.size());
+        System.out.println("\nВсего файлов, имеющих копии: " + sizeDoubles.size());
     }
 
     private String input() {
@@ -56,7 +48,7 @@ public class Searcher {
         }
     }
 
-    private List<File> iterationFilesFrom(String selectedDirectory) throws IOException {
+    private List<File> iterationFilesFrom(String selectedDirectory) {
         List<File> fileList = new ArrayList<>();
         try (Stream<Path> pathStream = Files.walk(Paths.get(selectedDirectory))) {
             fileList = pathStream.filter(Files::isRegularFile).map(Path::toFile).collect(Collectors.toList());
@@ -67,32 +59,25 @@ public class Searcher {
         return fileList;
     }
 
-    private List<List<Path>> findTimeDoubles(List<File> fileList) throws IOException {
-        Map<FileTime, List<Path>> timeFileMap = new HashMap<>();
-        List<Path> filePathList;
-        for (File file : fileList) {
-            FileTime fileTime = Files.getLastModifiedTime(file.toPath());
-            timeFileMap.putIfAbsent(fileTime, new ArrayList<>());
-            filePathList = timeFileMap.get(fileTime);
-            filePathList.add(file.toPath());
-            timeFileMap.put(fileTime, filePathList);
-        }
-        return timeFileMap.values().stream().filter(l -> l.size() > 1).collect(Collectors.toList());
+    private List<List<File>> getTimeDoubles(List<File> fileList) throws IOException {
+        Map<Long, List<File>> map = fileList.stream().collect(Collectors.groupingBy(File::lastModified));
+        return map.values().stream().filter(l -> l.size() > 1).collect(Collectors.toList());
     }
 
-    private List<List<List<Path>>> findSizeDoubles(List<List<Path>> timeDoubles) throws IOException {
-        List<List<List<Path>>> sizeDoubles = new ArrayList<>();
-        for (List<Path> oneTimeDoubles : timeDoubles) {
-            Map<Long, List<Path>> sizeMap = new HashMap<>();
-            List<Path> oneSizePaths;
-            for (Path path : oneTimeDoubles) {
-                long size = Files.size(path);
+    //TODO и это
+    private List<List<List<File>>> getSizeDoubles(List<List<File>> timeDoubles) throws IOException {
+        List<List<List<File>>> sizeDoubles = new ArrayList<>();
+        for (List<File> oneTimeDoubles : timeDoubles) {
+            Map<Long, List<File>> sizeMap = new HashMap<>();
+            List<File> oneSizePaths;
+            for (File file : oneTimeDoubles) {
+                long size = Files.size(file.toPath());
                 sizeMap.putIfAbsent(size, new ArrayList<>());
                 oneSizePaths = sizeMap.get(size);
-                oneSizePaths.add(path);
+                oneSizePaths.add(file);
                 sizeMap.put(size, oneSizePaths);
             }
-            List<List<Path>> sizes = sizeMap.values().stream().filter(l -> l.size() > 1).collect(Collectors.toList());
+            List<List<File>> sizes = sizeMap.values().stream().filter(l -> l.size() > 1).collect(Collectors.toList());
             sizeDoubles.add(sizes);
         }
         return sizeDoubles;
