0a2e846b3d91c059bd40946bea429315fc689261
agivdel <agph@yandex.ru>
2021-01-14 15:37:27 +0300

    correct Doubles class (delete field FileTime)

--- src/main/java/agivdel/copiedFilesSearch/Doubles.java
+++ src/main/java/agivdel/copiedFilesSearch/Doubles.java
@@ -1,37 +1,38 @@
 package agivdel.copiedFilesSearch;
 
+import java.io.File;
 import java.nio.file.attribute.FileTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Doubles {
-    private final FileTime fileTime;
-    private final List<String> doubles;
+//    private final FileTime fileTime;
+    private final List<File> doubles;
 
-    public Doubles(FileTime fileTime, List<String> doubles) {
-        this.fileTime = fileTime;
+    public Doubles(/*FileTime fileTime,*/ List<File> doubles) {
+//        this.fileTime = fileTime;
         this.doubles = doubles;
     }
 
-    public FileTime getFileTime() {
-        return fileTime;
-    }
+//    public FileTime getFileTime() {
+//        return fileTime;
+//    }
 
-    public List<String> getDoubles() {
+    public List<File> getDoubles() {
         return doubles;
     }
 
     @Override
     public String toString() {
         return "Double{" +
-                "fileTime=" + fileTime +
+                /*"fileTime=" + fileTime +*/
                 ", doubles=" + doubles +
                 '}';
     }
 
-    public static Doubles mergeNames(Doubles d1, Doubles d2) {
-        List<String> newDoublesList = new ArrayList<>(d1.getDoubles());
+    public static Doubles merge(Doubles d1, Doubles d2) {
+        List<File> newDoublesList = new ArrayList<>(d1.getDoubles());
         newDoublesList.addAll(d2.getDoubles());
-        return new Doubles(d1.getFileTime(), newDoublesList);
+        return new Doubles(/*d1.getFileTime(),*/ newDoublesList);
     }
 }
