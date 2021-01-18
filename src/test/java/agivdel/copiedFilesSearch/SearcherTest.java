cc0a19e138a7463008d64b5c1a43dd2843482f8a
agivdel <agph@yandex.ru>
2021-01-18 12:37:43 +0300

    add SearcherTest class with one test method for only public method of Searcher class

---
+++ src/test/java/agivdel/copiedFilesSearch/SearcherTest.java
@@ -0,0 +1,21 @@
+package agivdel.copiedFilesSearch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+public class SearcherTest {
+    String searchDir = "C:\\Users\\agivd\\JavaProjects\\copiedFilesSearch\\src\\main\\resources";
+
+    @Test
+    public void getDoublesList_Test() {
+        Searcher searcher = new Searcher();
+        Walker walker = new Walker();
+        List<File> fileList = walker.iterationFilesFrom(searchDir);
+
+        List<Doubles> doublesList = searcher.getDoublesList(fileList);
+        Assert.assertEquals(6, doublesList.size());
+    }
+}
