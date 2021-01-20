ca18ffb957ad6c2f0f88968415818103195965cf
agivdel <agph@yandex.ru>
2021-01-20 13:17:44 +0300

    rewrite all tests of TUITest

--- src/test/java/agivdel/copiedFilesSearch/WalkerTest.java
+++ src/test/java/agivdel/copiedFilesSearch/WalkerTest.java
@@ -1,27 +1,20 @@
 package agivdel.copiedFilesSearch;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.util.List;
 
 public class WalkerTest {
-    File testResources = new File("src/test/resources");
     Walker walker = new Walker();
-    List<File> fileList;
-
-    @Before
-    public void iteration() {
-        fileList = walker.iterationFilesFrom(testResources.toString());
-    }
 
     /**
      * обход директории поиска возвращает список всех ее файлов (не директорий), включая файлы во вложенных директориях
      */
     @Test
     public void iterationAllFiles_Test() {
+        List<File> fileList = walker.iterationFilesFrom("src/test/resources");
         Assert.assertEquals(27, fileList.size());
     }
 
@@ -30,7 +23,7 @@ public class WalkerTest {
      */
     @Test
     public void iterationEmptyDir_Test() {
-        fileList = walker.iterationFilesFrom(testResources.toString() + "/data/movie");
+        List<File> fileList = walker.iterationFilesFrom("src/test/resources/data/movie");
         Assert.assertTrue(fileList.isEmpty());
     }
 
@@ -39,6 +32,7 @@ public class WalkerTest {
      */
     @Test
     public void removeZeroSizeFiles_Test() {
+        List<File> fileList = walker.iterationFilesFrom("src/test/resources");
         fileList = walker.removeZeroSize(fileList);
         Assert.assertEquals(23, fileList.size());
     }
