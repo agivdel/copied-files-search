6dfbf66219749bb822b64d5c8b53659b87b887e1
agivdel <agph@yandex.ru>
2021-01-21 16:00:39 +0300

    delete sorting by creation time in Doubles constructor

--- src/main/java/agivdel/copiedFilesSearch/Doubles.java
+++ src/main/java/agivdel/copiedFilesSearch/Doubles.java
@@ -1,14 +1,7 @@
 package agivdel.copiedFilesSearch;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.FileTime;
 import java.util.List;
-
-import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toList;
 
 /**
  * Объект класса Double хранит список файлов, сортированных по времени создания
@@ -18,21 +11,11 @@ public class Doubles {
     private final List<File> doubles;
 
     public Doubles(List<File> files) {
-        this.doubles = files.stream()
-                .sorted(comparing(this::getCreateTime))
-                .collect(toList());
+        this.doubles = files;
     }
 
     public List<File> getDoubles() {
         return doubles;
     }
 
-    private FileTime getCreateTime(File file) {
-        try {
-            return Files.readAttributes(file.toPath(), BasicFileAttributes.class).creationTime();
-        } catch (IOException e) {
-            System.err.println("IO error");
-            throw new RuntimeException(e);
-        }
-    }
 }
