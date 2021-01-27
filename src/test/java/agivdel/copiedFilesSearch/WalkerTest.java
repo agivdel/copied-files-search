2c674d3aca38867df8e19cc533394cc66e43b3dd
agivdel <agph@yandex.ru>
2021-01-27 14:58:49 +0300

    rewrite WalkerTest class for work with List<Forms> in all its methods

--- src/test/java/agivdel/copiedFilesSearch/WalkerTest.java
+++ src/test/java/agivdel/copiedFilesSearch/WalkerTest.java
@@ -2,19 +2,40 @@ package agivdel.copiedFilesSearch;
 
 import org.junit.Assert;
 import org.junit.Test;
-import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class WalkerTest {
     Walker walker = new Walker();
+
+    @Test
+    public void test0() throws IOException {
+//        FileSystem fileSystem = Jimfs.newFileSystem();
+//        String fileName = "newFile.txt";
+//        String fileName2 = "newFile2.txt";
+//        String fileName3 = "newFile3.txt";
+//        Path pathToStore = fileSystem.getPath("");
+//        FileRepository fileRepository = new FileRepository();
+//        fileRepository.create(pathToStore, fileName);
+//        fileRepository.create(pathToStore, fileName2);
+//        Assert.assertTrue(Files.exists(pathToStore.resolve(fileName)));
+//        Assert.assertTrue(Files.exists(pathToStore.resolve(fileName2)));
+//        System.out.println(fileName);
+//        System.out.println(Files.getLastModifiedTime(Path.of(fileName)));
+//        Assert.assertTrue(Files.exists(pathToStore.resolve(fileName3)));
+//        Stream<Path> pathStream = Files.walk(pathToStore);
+//        System.out.println(pathStream.count());
+//        System.out.println(Files.exists(Paths.get(fileName)));
+//        System.out.println(Files.exists(Paths.get(fileName2)));
+    }
 
     /**
      * обход директории поиска возвращает список всех ее файлов (не директорий), включая файлы во вложенных директориях
      */
     @Test
     public void iterationAllFiles_Test() {
-        List<File> fileList = walker.iterationFilesFrom("src/test/resources");
-        Assert.assertEquals(27, fileList.size());
+        List<Forms> files = walker.iterationFilesFrom("src/test/resources");
+        Assert.assertEquals(27, files.size());
     }
 
     /**
@@ -22,8 +43,8 @@ public class WalkerTest {
      */
     @Test
     public void iterationEmptyDir_Test() {
-        List<File> fileList = walker.iterationFilesFrom("src/test/resources/data/movie");
-        Assert.assertTrue(fileList.isEmpty());
+        List<Forms> files = walker.iterationFilesFrom("src/test/resources/data/movie");
+        Assert.assertTrue(files.isEmpty());
     }
 
     /**
@@ -31,8 +52,8 @@ public class WalkerTest {
      */
     @Test
     public void removeZeroSizeFiles_Test() {
-        List<File> fileList = walker.iterationFilesFrom("src/test/resources");
-        fileList = walker.removeZeroSize(fileList);
-        Assert.assertEquals(23, fileList.size());
+        List<Forms> files = walker.iterationFilesFrom("src/test/resources");
+        files = walker.removeZeroSizeForm(files);
+        Assert.assertEquals(23, files.size());
     }
 }
