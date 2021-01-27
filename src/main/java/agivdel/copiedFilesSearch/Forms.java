a453ded7bded768429f32aa584ffa5119fe12a88
agivdel <agph@yandex.ru>
2021-01-27 14:56:38 +0300

    add interface Forms and two its implementations: WorkForm class and TestForm class

---
+++ src/main/java/agivdel/copiedFilesSearch/Forms.java
@@ -0,0 +1,10 @@
+package agivdel.copiedFilesSearch;
+
+import java.nio.file.Path;
+
+public interface Forms {
+    Path toPath();
+    long size();
+    long lastModified();
+    long getCRC32();
+}
---
+++ src/test/java/agivdel/copiedFilesSearch/TestForm.java
@@ -0,0 +1,74 @@
+package agivdel.copiedFilesSearch;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class TestForm implements Forms {
+    private final String name;
+    private final long size;
+    private final long createTime;
+    private long lastModifiedTime;
+
+    public TestForm(String name, long size) {
+        this.name = name;
+        this.size = size;
+        this.createTime = setCreateTime();
+    }
+
+    public TestForm(String name, long size, long time) {
+        this.name = name;
+        this.size = size;
+        this.createTime = setCreateTime();
+        this.lastModifiedTime = time;
+    }
+
+    @Override
+    public Path toPath() {
+        return Paths.get(this.name);
+    }
+
+    @Override
+    public long size() {
+        return this.size;
+    }
+
+    @Override
+    public long lastModified() {
+        return this.lastModifiedTime;
+    }
+
+    @Override
+    public long getCRC32() {
+        return this.size() * 31 + 17;
+    }
+
+    public static TestForm copy(TestForm oldForm) {
+        TestForm newForm = new TestForm(oldForm.name + " - копия", oldForm.size());
+        long time = oldForm.createTime == oldForm.lastModifiedTime ?
+                oldForm.createTime :
+                oldForm.lastModifiedTime;
+        oldForm.setLastModified(time);
+        newForm.setLastModified(time);
+        return newForm;
+    }
+
+    private long setCreateTime() {
+        return LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond();
+    }
+
+    private void setLastModified(long time) {
+        this.lastModifiedTime = time;
+    }
+
+    @Override
+    public String toString() {
+        return "TestForm{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                ", createTime=" + createTime +
+                ", lastModifiedTime=" + lastModifiedTime +
+                "}";
+    }
+}
