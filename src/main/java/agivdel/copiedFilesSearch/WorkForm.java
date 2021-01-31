a5468b985627473bc03b83e77e908e349e3b6c42
agivdel <agph@yandex.ru>
2021-01-31 13:41:58 +0300

    add Suppliers.memoize()

--- src/main/java/agivdel/copiedFilesSearch/WorkForm.java
+++ src/main/java/agivdel/copiedFilesSearch/WorkForm.java
@@ -1,16 +1,20 @@
 package agivdel.copiedFilesSearch;
 
+import com.google.common.base.Suppliers;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.attribute.FileTime;
+import java.util.function.Supplier;
 import java.util.zip.CRC32;
 
 public class WorkForm implements Forms{
     private final Path path;
     private final long size;
     private final long lastModifiedTime;
+    private final Supplier<Long> crc32supplier = Suppliers.memoize(this::getCRC32private)::get;
 
     public WorkForm(Path path, long size, long lastModifiedTime) {
         this.path = path;
@@ -35,6 +39,10 @@ public class WorkForm implements Forms{
 
     @Override
     public long getCRC32() {
+        return crc32supplier.get();
+    }
+
+    private long getCRC32private() {
         CRC32 check = new CRC32();
         byte[] buf = new byte[8192];
         try (FileInputStream fis = new FileInputStream(this.toPath().toFile())) {
